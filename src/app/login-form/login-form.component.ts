ce4cbe03e90b8fa174cdf83d1e04daab0eb59026
Milan <milancakic89@gmail.com>
2020-10-07 10:55:24 +0200

    Login component finished

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Service } from '../app.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { BASE_URL } from '../base-url';
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+  submiting = false;
+  constructor(private http: HttpClient,
+    private service: Service,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  submit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const name = form.value.name;
+    const email = form.value.email;
+    const password = form.value.password;
+    const repeatPassword = form.value.repeatPassword;
+    if (password !== repeatPassword) {
+      this.service.emitMessage.emit('Password fields do not match');
+      return;
+    }
+    this.submiting = true;
+    this.http.post(BASE_URL.getBaseUrl() + '/signup', { email: email, password: password, name: name })
+      .subscribe((user: { success: boolean, message: string }) => {
+        this.submiting = false;
+        if (user.success) {
+          this.service.success.emit(true);
+          this.service.emitMessage.emit(user.message)
+          setTimeout(() => {
+            return this.router.navigate(['/'])
+          }, 1500)
+
+        } else {
+          this.service.success.emit(false);
+          return this.service.emitMessage.emit(user.message);
+        }
+
+      },
+        error => {
+          this.service.success.emit(true);
+          this.service.emitMessage.emit(error)
+        })
   }
-
 }
