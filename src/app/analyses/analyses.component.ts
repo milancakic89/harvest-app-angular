36b1038a450adbe5e597f4210fd67e9a945aa3cd
Milan <milancakic89@gmail.com>
2020-10-07 13:32:15 +0200

    Analyses component complete

--- src/app/analyses/analyses.component.ts
+++ src/app/analyses/analyses.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Service } from '../app.service';
+import { BASE_URL } from '../base-url';
 
 @Component({
   selector: 'app-analyses',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysesComponent implements OnInit {
 
-  constructor() { }
+  sectors;
+  total = 0;
+
+  constructor(private http: HttpClient,
+    private service: Service) { }
 
   ngOnInit(): void {
+    const token = this.service.getToken();
+    this.http.get(BASE_URL.getBaseUrl() + '/analyse', {
+      headers: {
+        Autorization: token
+      }
+    })
+      .subscribe((data: { sectors, total }) => {
+        if (Array.isArray(data.sectors) && data.sectors.length > 0) {
+          this.total = data.total.toFixed(4);
+          const sectorsData = data.sectors.sort((a, b) => {
+            if (a.total > b.total) {
+              return -1;
+            } else if (b.total > a.total) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+          this.sectors = sectorsData;
+        }
+
+      },
+        error => this.service.emitMessage.emit(error))
   }
 
 }
