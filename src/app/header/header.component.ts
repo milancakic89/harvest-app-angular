8d93ea7fb68b44440c00db91af27e784076b40d4
Milan <milancakic89@gmail.com>
2020-10-07 13:36:13 +0200

    Header finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { Service } from '../app.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
+  menuOpened = false;
+  isLogged = false;
+  user = '';
+  constructor(private service: Service) { }
 
-  constructor() { }
-
+  ngOnChanges() {
+    this.service.checkToken();
+    this.isLogged = !!localStorage.getItem('loggedIn');
+    if (!this.isLogged) {
+      this.logout();
+    }
+  }
   ngOnInit(): void {
+    this.user = this.service.getUserName();
+     this.isLogged = !!localStorage.getItem('loggedIn');
+
   }
 
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+  }
+
+  logout() {
+    this.service.logout();
+  }
 }
