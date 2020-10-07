dc451bb9ee5cc322d4c53ef705dbcdad0ec71ff2
Milan <milancakic89@gmail.com>
2020-10-07 13:31:01 +0200

    Base component complete

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Service } from '../app.service';
+import { BASE_URL } from '../base-url';
 
 @Component({
   selector: 'app-base',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base.component.css']
 })
 export class BaseComponent implements OnInit {
-
-  constructor() { }
+  boxes: [];
+  constructor(private http: HttpClient,
+    private service: Service) { }
 
   ngOnInit(): void {
+    const token = this.service.getToken();
+    this.http.get(BASE_URL.getBaseUrl() + '/base', {
+      headers: {
+        Autorization: token
+      }
+    })
+      .subscribe((data: { boxes: [] }) => {
+        this.boxes = data.boxes;
+      })
   }
-
 }
