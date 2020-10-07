042e4999a8718719636b1eb83f1e1f5ba3568919
Milan <milancakic89@gmail.com>
2020-10-07 13:46:04 +0200

    Sub-header component complete

--- src/app/measurement/sub-header/sub-header.component.ts
+++ src/app/measurement/sub-header/sub-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Service } from 'src/app/app.service';
 
 @Component({
   selector: 'app-sub-header',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubHeaderComponent implements OnInit {
 
-  constructor() { }
+  menuOpened = false;
+  isLogged = false;
+  user = '';
+  constructor(private service: Service) { }
 
+  ngOnChanges() {
+    this.service.checkToken();
+    this.isLogged = !!localStorage.getItem('loggedIn');
+    if (!this.isLogged) {
+      this.service.logout();
+    }
+  }
   ngOnInit(): void {
+    this.user = this.service.getUserName();
+    this.isLogged = !!localStorage.getItem('loggedIn');
+
   }
 
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+  }
+
+  logout() {
+    this.isLogged = false;
+    this.service.logout();
+  }
 }
