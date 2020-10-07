09c3414fe5ea860ed676152cad1ab497a234a369
Milan <milancakic89@gmail.com>
2020-10-07 13:45:01 +0200

    Barcode manual component finished

--- src/app/measurement/barcode-manual/barcode-manual.component.ts
+++ src/app/measurement/barcode-manual/barcode-manual.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+import { BoundText } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Service } from 'src/app/app.service';
+import { BASE_URL } from 'src/app/base-url';
 
 @Component({
   selector: 'app-barcode-manual',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./barcode-manual.component.css']
 })
 export class BarcodeManualComponent implements OnInit {
-
-  constructor() { }
+  success = false;
+  barcodeData;
+  employees;
+  selected = '';
+  constructor(private service: Service, private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const token = this.service.getToken();
+    this.route.params.subscribe((params: Params) => {
+      const barcode = params['barcode'];
+      this.http.get(BASE_URL.getBaseUrl() + `/barcode/${barcode}`, {
+        headers: {
+          Autorization: token
+        }
+      })
+        .subscribe((data: { box, employees }) => {
+          this.selected = data.box.employee;
+          this.employees = data.employees.filter(employee => {
+            return employee.name != data.box.employee;
+          });
+             this.service.success.emit(true);
+          this.service.emitMessage.emit('Success');
+          this.barcodeData = data;
+          this.success = true;
+        },
+          error => {
+               this.service.success.emit(false);
+               this.service.emitMessage.emit(error)
+          })
+    })
   }
 
 }
